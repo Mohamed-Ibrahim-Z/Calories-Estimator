8dc4270c2ab5de1e3de23e09a29cecd4a8aa7450
Square <square@pop-os.localdomain>
2023-06-25 18:06:13 +0300

    id card segmentation fixed

--- Test Script/ID_card Segmentation/Id_Card Segmentation.py
+++ Test Script/ID_card Segmentation/Id_Card Segmentation.py
@@ -7,49 +7,78 @@ def angle_cos(p0, p1, p2):
     return abs( np.dot(d1, d2) / np.sqrt( np.dot(d1, d1)*np.dot(d2, d2) ) )
 
 def getIdCard(img):
-    img = cv2.GaussianBlur(img, (5, 5), 0)
-    rects = []
+
+    imgArea =img.shape[0] * img.shape[1]
+
+    img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    channels_hsv = cv2.split(img_hsv)
+
+    channel_s = channels_hsv[1]
+    channel_s = cv2.GaussianBlur(channel_s, (9, 9), 2, 2)
+
+    imf = channel_s.astype(np.float32)
+    imf = cv2.convertScaleAbs(imf, alpha=0.5, beta=0.5)
+
+    sobx = cv2.Sobel(imf, cv2.CV_32F, 1, 0)
+    soby = cv2.Sobel(imf, cv2.CV_32F, 0, 1)
+    sobx = cv2.multiply(sobx, sobx) 
+    soby = cv2.multiply(soby, soby)
+
+    grad_abs_val_approx = cv2.pow(sobx + soby, 0.5)    
+    filtered = cv2.GaussianBlur(grad_abs_val_approx, (9, 9), 2, 2)
+    
+    img=cv2.cvtColor((filtered).astype(np.uint8), cv2.COLOR_RGB2BGR)
+    
+
+    # Adjust the kernel size for desired thickness
+    kernel = np.ones((3, 3), dtype=np.uint8)  
+    # Dilate the edges
+    img = cv2.dilate(img, kernel, iterations=3)
+
+    rects = []  
     for gray in cv2.split(img):
         for thrs in range(0, 255, 26):
             if thrs == 0:
                 bin = cv2.Canny(gray, 0, 50, apertureSize=5)
             else:
                 retval, bin = cv2.threshold(gray, thrs, 255, cv2.THRESH_BINARY+ cv2.THRESH_OTSU)
-
+            
             contours, hierarchy = cv2.findContours(bin, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 
             for cnt in contours:
                 cnt_len = cv2.arcLength(cnt, True)
                 cnt = cv2.approxPolyDP(cnt, 0.02*cnt_len, True)
-                if len(cnt) == 4 and cv2.contourArea(cnt) > 1000 and cv2.isContourConvex(cnt):
+                cntArea = cv2.contourArea(cnt)
+                if len(cnt) == 4 and  cntArea > 1000 and cv2.isContourConvex(cnt):
                     cnt = cnt.reshape(-1, 2)
                     max_cos = np.max([angle_cos(cnt[i], cnt[(i+1) % 4], cnt[(i+2) % 4] ) for i in range(4)])
-                    if max_cos < 0.1:
+                    if max_cos < 0.1 and cntArea/imgArea<0.75:
                         rects.append(cnt)
 
     largest_contour = max(rects, key=cv2.contourArea)
+
 
     mask = np.zeros_like(img)  # create blank image of same size as input
     cv2.drawContours(img, [largest_contour], -1, (0, 255, 0), 1)
     cv2.fillPoly(mask, [largest_contour], (255, 255, 255))  # draw square on mask
     masked = cv2.bitwise_and(img, mask)  # apply mask to input image
+    
+    
 
     cv2.imwrite('IdCard.png', img)
     cv2.imwrite('maskImg.png', mask)
     cv2.imwrite('maskedImg.png', masked)
 
     gray_img = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
+
     # Threshold the image to get the white pixels
     ret, thresh_img = cv2.threshold(gray_img, 127, 255, cv2.THRESH_BINARY)
+
     # Count the number of white pixels
     num_white_pixels = cv2.countNonZero(thresh_img)
     return largest_contour,num_white_pixels
 
 if __name__ == '__main__':
-    img = cv2.imread('Orignal.jpg')
+    img = cv2.imread('p5.jpg')
     idCard,whitePixels = getIdCard(img)
     print(whitePixels)
-
-
-
-
